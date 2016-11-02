2af0f3b3dbbd8313f77244be0d0cb6600d6030df
Crauzer <thenatokenproject@gmail.com>
2016-11-02 18:34:37 +0100

    Add project files.

---
+++ Sabre-WGT-Parser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabre_WGT_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WGTFile w = new WGTFile("Annie.wgt");
+
+            Console.WriteLine("> Magic : " + w.hd.Magic);
+            Console.WriteLine("> Version : " + w.hd.Version);
+            Console.WriteLine("> ID : " + w.hd.ID);
+            Console.WriteLine("> Weight Count : " + w.hd.WeightCount);
+            Console.WriteLine();
+            foreach(var we in w.Weights)
+            {
+                Console.WriteLine("> ID : " + we.BoneID);
+                Console.WriteLine("> Weight : " + we.WeightValue[0] + ", " + we.WeightValue[1] + ", " + we.WeightValue[2]);
+                Console.WriteLine("> Zero : " + we.Zero);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sabre-WGT-Parser/WGTFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sabre_WGT_Parser
+{
+    class WGTFile
+    {
+        public BinaryReader br;
+        public Header hd;
+        public List<Weight> Weights = new List<Weight>();
+        public WGTFile(string fileLocation)
+        {
+            br = new BinaryReader(File.Open(fileLocation, FileMode.Open));
+            hd = new Header(br);
+            for(int i = 0; i < hd.WeightCount; i++)
+            {
+                Weights.Add(new Weight(br));
+            }
+        }
+        public class Header
+        {
+            public string Magic;
+            public UInt32 Version;
+            public UInt32 ID;
+            public UInt32 WeightCount;
+            public Header(BinaryReader br)
+            {
+                Magic = Encoding.ASCII.GetString(br.ReadBytes(8));
+                Version = br.ReadUInt32();
+                ID = br.ReadUInt32();
+                WeightCount = br.ReadUInt32();
+            }
+        }
+        public class Weight
+        {
+            public UInt32 BoneID;
+            public float[] WeightValue = new float[3];
+            public UInt32 Zero;
+            public Weight(BinaryReader br)
+            {
+                BoneID = br.ReadUInt32();
+                for(int i = 0; i < 3; i++)
+                {
+                    WeightValue[i] = br.ReadSingle();
+                }
+                Zero = br.ReadUInt32();
+            }
+        }
+    }
+}
